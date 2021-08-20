0ed5b82234e9ace4df20d7b34aa2432870cf2e34
debashismazumderbd <debashismazumder.bd@gmail.com>
2021-08-20 19:17:33 +0600

    function error solved

--- js/main.js
+++ js/main.js
@@ -3,25 +3,69 @@ const div2 = document.getElementById("second");
 const div3 = document.getElementById("third");
 
 const img = document.getElementById("art")
-
+// description & price
 const desc = document.getElementById("desc");
 const price = document.getElementById("price");
 
+// shipping var
+
+const free = document.getElementById("free");
+const express = document.getElementById("express")
+
+const shippingField = document.getElementById("shipping");
+const total = document.getElementById("total");
+// shipping event listener
+free.addEventListener("click", function(){
+    shippingField.innerText = "5";
+    updateTotal();
+});
+
+// express shipping
+express.addEventListener("click", function(){
+    shippingField.innerText = "30";
+    updateTotal();
+});
+
+// update total 
+function updateTotal(){
+    const shippingCharge = Number(shippingField.innerText);
+    const productPrice = Number(price.innerText);
+    const grandTotal = shippingCharge + productPrice;
+    total.innerText = grandTotal;
+}
 
 div1.addEventListener("click", function() {
+    // changing img
     img.src = "images/1.jpg";
+    // changing description
     desc.innerText = "First-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, veniam ad soluta est, corrupti repellat temporibus provident accusamus deleniti facere non assumenda? Pariatur, expedita ipsum?";
-    price.innerText = "750$";
+    // price
+    price.innerText = "750";
+    //update total
+    updateTotal();
+
 })
 div2.addEventListener("click", function() {
+    // changing img
+
     img.src = "images/2.jpg";
+    // changing description
+
 
     desc.innerText = "First-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, veniam ad soluta est, corrupti repellat temporibus provident accusamus deleniti facere non assumenda? Pariatur, expedita ipsum?";
-    price.innerText = "550$";
+    // changing price
+    price.innerText = "550";
+    // update total 
+    updateTotal();
 })
 div3.addEventListener("click", function() {
+    // image change
     img.src = "images/3.jpg";
+    // description
 
     desc.innerText = "First-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, veniam ad soluta est, corrupti repellat temporibus provident accusamus deleniti facere non assumenda? Pariatur, expedita ipsum?";
-    price.innerText = "330$";
+    // price
+    price.innerText = "330";
+    // update total
+    updateTotal();
 })
